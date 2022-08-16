5efed6ac6d6f4bca5fe0321b2103635970fc40c6
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2022-08-16 22:20:35 +0300

    PathVariable

--- src/main/java/com/example/springjpamysql/controller/EmployeeController.java
+++ src/main/java/com/example/springjpamysql/controller/EmployeeController.java
@@ -4,15 +4,20 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-//@Controller // handles Http requests
-@RestController // @Controller + @ResponseBody
+// handles the http request
+@RestController
 public class EmployeeController {
 
     // localhost:8080/employees
-    //@RequestMapping(value = "/employees", method = RequestMethod.GET)
-    //@ResponseBody // if we use only @Controller @RequestMapping
-    @GetMapping("/employees") // instead of
+    @GetMapping("/employees")
     public String getEmployees(){
         return "displaying list of employees";
     }
+
+    // localhost:8080/employees/73
+    @GetMapping("/employees/{id}")
+    public String getEmployee(@PathVariable("id") Long id){
+        return "Fetching the employee details for the id " + id;
+    }
+
 }
