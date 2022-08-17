a80c551c1297905bd6f8668167eacb8b6c294a95
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2022-08-17 10:28:06 +0300

    JsonIgnore and JsonProperty

--- src/main/java/com/example/springjpamysql/model/Employee.java
+++ src/main/java/com/example/springjpamysql/model/Employee.java
@@ -1,5 +1,7 @@
 package com.example.springjpamysql.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,7 +11,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Employee {
+    @JsonProperty("full_name") // changed name, inside the model class it will be name
     private String name;
+
+    // hiding info in http request
+    @JsonIgnore // will not return in json, age is safe
     private Long age;
     private String location;
     private String email;
